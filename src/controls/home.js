79a058a9ed645cc290bf094a35c692042ceb4c19
Josue Villasante <javillasantet@gmail.com>
2017-09-21 00:19:35 -0500

    can now save new word

--- src/controls/home.js
+++ src/controls/home.js
@@ -3,5 +3,18 @@ import firebase from 'firebase/app'
 export default class Home {
     start () {
         $('.button-collapse').sideNav()
+        $('#modal-new-word').modal()
+
+        $('#btn-add-word').click(() => this.addWord())
+    }
+
+    addWord () {
+        firebase.database().ref('words').push()
+            .set({
+                name: $('#word').val()
+            })
+            .then(() => {
+                Materialize.toast('Se agrego correctamente la palabra', 4000)
+            })
     }
 }
